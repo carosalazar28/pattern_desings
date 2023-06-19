148a3ba9f7085a9d27e23218873fe96ef0382d02
Carolina Salazar <cindy.salazar@mercadolibre.com.co>
2023-06-18 21:14:17 -0500

    se agrega el patron builder

---
+++ builder/index.js
@@ -0,0 +1,75 @@
+class Person {
+  constructor(name, lastName, age, country, city, hobbies) {
+    this.name = name
+    this.lastName = lastName
+    this.age = age
+    this.country = country
+    this.city = city
+    this.hobbies = hobbies
+  }
+
+  getFullName() {
+    return `${this.name} ${this.lastName}`
+  }
+}
+
+class PersonBuilder {
+  constructor () {
+    this.reset()
+  }
+
+  reset() {
+    this.name = ''
+    this.lastName = ''
+    this.age = 0
+    this.country = ''
+    this.city = ''
+    this.hobbies = []
+  }
+
+  setName(name) {
+    this.name = name
+    return this // Esto es para encadenar los metodos
+  }
+  setLastName(lastName) {
+    this.lastName = lastName
+    return this
+  }
+  setAge(age) {
+    this.age = age
+    return this
+  }
+  setCountry(country) {
+    this.country = country
+    return this
+  }
+  setCity(city) {
+    this.city = city
+    return this
+  }
+  addHobbie(hobbie) {
+    this.hobbies.push(hobbie)
+    return this
+  }
+
+  build() {
+    const person = new Person(
+      this.name,
+      this.lastName, 
+      this.age,
+      this.country, 
+      this.city,
+      this.hobbies 
+    )
+    this.reset()
+    return person
+  }
+}
+
+const personBuilder = new PersonBuilder()
+const carolina = personBuilder.setName('Carolina')
+                              .setLastName('Salazar')
+                              .addHobbie('Correr')
+                              .build()
+
+console.log(carolina)
